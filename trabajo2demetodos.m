6a6a7a39d72407efb04257484a13b15befb11206
FrancoDelSole <56172509+FrancoDelSole@users.noreply.github.com>
2019-10-10 11:22:28 -0300

    Agrege LLS2
    Ahi agrege el segundo metodo de minimos cuadrado..
    
    Cosas que faltan:
    Yo hice  la minimisacion pero deje de lado el error por ruido blanco que despues hay que agregar eso que es mas matematica que otra cosa pero lo dejo aca para tener en cuenta

--- trabajo2demetodos.m
+++ trabajo2demetodos.m
@@ -18,6 +18,9 @@ clc
 %          -                 AGENTE          -     
 %          -                                 -
 %   ANTENA TOA (0,0) ------------------- ANTENA TOA (L,0)
+%APARTADO GRAFICO - O VISUAL PARA ENTENDER LO QUE CALCULAMOS
+imagen = imshow('grafico.bmp');
+clc
 %
 % El valor de L propuesto por el paper es de 10 metros
     
@@ -43,26 +46,32 @@ L=10;
 % --------------------------------------------------------------------------------------------
 % Calculo de distancia por cada antena:
 % -------------------TOA1---------------------------------------------------------------------
-Trtt =  4*10^(-8);          %Tiempo de ida y vuelta en la medicion de distancia
+Trtt1 =  4*10^(-8);          %Tiempo de ida y vuelta en la medicion de distancia
 Ttat =  7.14*10^(-9) ;         %Tiempo de respuesta del agente 
 
-d(1) = 3*10^8 * ( Trtt - Ttat )/2;
-%----Hay que agregar un error aca que tiene el calculo de toa por interrupcion de ruido blanco
-%---------------------TOA2--------------------------------------------------------------------
-Trtt =  4*10^(-8) ;         %Tiempo de ida y vuelta en la medicion de distancia
-Ttat =  7.14*10^(-9)  ;        %Tiempo de respuesta del agente 
+d(1) = 3*10^8 * ( Trtt1 - Ttat )/2;
 
-d(2) = 3*10^8 * ( Trtt - Ttat )/2;
 %----Hay que agregar un error aca que tiene el calculo de toa por interrupcion de ruido blanco
+
+%---------------------TOA2--------------------------------------------------------------------
+Trtt2 =  4*10^(-8);         %Tiempo de ida y vuelta en la medicion de distancia
+Ttat =  7.14*10^(-9) ;        %Tiempo de respuesta del agente 
+
+d(2) = 3*10^8 * ( Trtt2 - Ttat )/2;
+%----Hay que agregar un error aca que tiene el calculo de toa por interrupcion de ruido blanco
+
 %-----------------------Rss1-------------------------------------------------------------------
 %
 % Aca le voy a poner por el momento lo que dice el paper que mas o menos da un resultado de 10m
 %pero habria que calcular un di por medio de la potencia
 d(3)=sqrt(50);
+
+
 %-----------------------Rss2------------------------------------------------------------------
 % Aca le voy a poner por el momento lo que dice el paper que mas o menos da un resultado de 10m
 %pero habria que calcular un di por medio de la potencia
 d(4)=sqrt(50);
+
 %---------------------------------------------------------------------------------------------
 
 
@@ -84,18 +93,49 @@ y = [ 0 0 L L ];
         b(i,1) = d(i)^2 - x(i)^2 - y(i)^2;
     end 
 
+disp('------------------Red Hibrida LLS1----------------------------------')
 
+%Paso la matriz a sparce para ahorro de almasenamiento
+
+    sparse(A);
+    sparse(b);
+
+%Calculo directamente los minimos cuadrados a partir de la variable ficticia
 %Siendo F lo que nos linealiza el sistema para que podamos resolver, es
-%entonces nuestra variable a encontrar
-%------------------------------------------------------------------------------------------------
+%entonces nuestra variable a encontrar:
+%F tendra 3 componentes, 2 son las cordenadas, las primeras dos
 
-%---------------------------------------------------------------------------------------------
-%
-%Paso la matriz a una formato ralo
-%
+    Fsol = inv((A'*A))*A'*b; 
+    plls1 = [Fsol(1), Fsol(2)];
+
+disp('Distancia en metros desde antena de referencia ubicada en (0,0)')
+%Luego tomo la distancia como:
+dis = sqrt(plls1(1)^2+plls1(2)^2)
+dcord = [Fsol(1), Fsol(2)]
+disp('Angulo en grados')
+angle = atan(plls1(2)/plls1(1))*360/(2*pi)
+
+
+%Repetire el proceso para  ahora tener en cuenta 4 toa entonces remplazare las 2 que eran
+%rss por toa
+
+    Trtt3 =  4*10^(-8); 
+    d(3) = 3*10^8 * ( Trtt3 - Ttat )/2;
+
+    Trtt4 =  4*10^(-8); 
+    d(4) = 3*10^8 * ( Trtt4 - Ttat )/2;
+
+
+    for i = 1:4
+        A(i,1) = [ -2 * x(i) ];
+        A(i,2) = [ -2 * y(i) ];
+        A(i,3) = 1;
+        b(i,1) = d(i)^2 - x(i)^2 - y(i)^2;
+    end 
+
 sparse(A);
 sparse(b);
-%
+
 %Realizo un calculo propuesto por el paper para encontrar el valor de la
 %variable, que es despejar la variable
 
@@ -109,8 +149,9 @@ plls1 = [Fsol(1), Fsol(2)]; %p de posicion
 %--------------------------------------------------------------------------------------------
 %Luego tomo la distancia como:
 %
+disp('---------------------Red no hibrida LLS1---------------------------------')
 disp('Distancia en metros desde antena de referencia ubicada en (0,0)')
-d = sqrt(plls1(1)^2+plls1(2)^2)
+dis = sqrt(plls1(1)^2+plls1(2)^2)
 dcord = [Fsol(1), Fsol(2)]
 disp('Angulo en grados')
 angle = atan(plls1(2)/plls1(1))*360/(2*pi)
@@ -119,4 +160,60 @@ angle = atan(plls1(2)/plls1(1))*360/(2*pi)
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------Segundo metodo LLS2------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%Para el segundo metodo nececitamos fijar alguna antena como referencia
+%EL paper nos propone 4 formas, para probar el metodo utilizo tomar una antena aleatoreamente
+%supongamos que esa va a ser la ubicada en (0,0)
+%
+    xr = 0; yr= 0;
+    kr = [ xr^2 + yr^2 ];
+    for i = 1:4
+        A2(i,1) = [ 2 * ( x(i) - xr ) ];
+        A2(i,2) = [ 2 * ( y(i) - yr )];
+        k(i,1) = [ x(i)^2 + y(i)^2 ];
+        b2(i,1) = d(1)^2 - d(i)^2 - kr - k(i,1);
+    end 
 
+%Ahora deberia hacer Aii * p = bii donde p tiene dos componentes, x e y
+%que seran las cordenadas
+
+Fsol = inv((A2'*A2))*A2'*b2; 
+plls1 = [Fsol(1), Fsol(2)]; %p de posicion
+
+
+disp('---------------------Red no hibrida LLS2---------------------------------')
+disp('Distancia en metros desde antena de referencia ubicada en (0,0)')
+dis = sqrt(plls1(1)^2+plls1(2)^2)
+dcord = [Fsol(1), Fsol(2)]
+disp('Angulo en grados')
+angle = atan(plls1(2)/plls1(1))*360/(2*pi)
+
+%Datos de antenas RSS
+d(4)=sqrt(50);d(3)=sqrt(50);
+
+%repito proceso
+xr = 0; yr= 0;
+kr = [ xr^2 + yr^2 ];
+for i = 1:4
+    A2(i,1) = [ 2 * ( x(i) - xr ) ];
+    A2(i,2) = [ 2 * ( y(i) - yr )];
+    k(i,1) = [ x(i)^2 + y(i)^2 ];
+    b2(i,1) = d(1)^2 - d(i)^2 - kr - k(i,1);
+end 
+
+%Ahora deberia hacer Aii * p = bii donde p tiene dos componentes, x e y
+%que seran las cordenadas
+
+Fsol = inv((A2'*A2))*A2'*b2; 
+plls1 = [Fsol(1), Fsol(2)]; %p de posicion
+
+
+disp('---------------------Red hibrida LLS2---------------------------------')
+disp('Distancia en metros desde antena de referencia ubicada en (0,0)')
+dis = sqrt(plls1(1)^2+plls1(2)^2)
+dcord = [Fsol(1), Fsol(2)]
+disp('Angulo en grados')
+angle = atan(plls1(2)/plls1(1))*360/(2*pi)
